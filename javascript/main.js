915f8c83b41986a9047f3702ce25411b98e534ab
nicolas <nicroraza@gmail.com>
2025-04-09 02:35:55 +0300

    new products develop

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,93 @@
-let slideIndex = 0;
+import {products} from '../data/data-products.js';
 
-function showSlide(index) {
-  const slides = document.getElementsByClassName("slide");
-  if (index >= slides.length) slideIndex = 0;
-  if (index < 0) slideIndex = slides.length - 1;
 
-  for (let slide of slides) {
-    slide.style.display = "none";
+document.addEventListener('DOMContentLoaded', function() {
+
+  // SLIDESHOW
+  const prev = document.querySelector(".prev");
+  const next = document.querySelector(".next");
+  let slideIndex = 0;
+  showSlide(slideIndex);
+  
+  prev.addEventListener("click", function() {
+    plusSlides(-1);
+  });
+
+  next.addEventListener("click", function() {
+    plusSlides(1);
+  });
+
+  function showSlide(index) {
+    const slides = document.getElementsByClassName("slide");
+    if (index >= slides.length) slideIndex = 0;
+    if (index < 0) slideIndex = slides.length - 1;
+  
+    for (let slide of slides) {
+      slide.style.display = "none";
+    }
+  
+    slides[slideIndex].style.display = "block";
+  }
+  
+  function plusSlides(n) {
+    slideIndex += n;
+    showSlide(slideIndex);
   }
 
-  slides[slideIndex].style.display = "block";
-}
 
-function plusSlides(n) {
-  slideIndex += n;
-  showSlide(slideIndex);
-}
+  // NEW PRODUCTS SETUP
+
+
+  let productsHTML = '';
+
+  products.forEach((product) => {
+    productsHTML += `
+      <div class="product">
+
+        <div class="productFavoriteContainer">
+          <img class="productFavoriteIcon" src="../images/product_heart.png" alt="favorite">
+        </div>
+
+        <div class="productImageContainer">
+          <img class="productImage"
+            src="${product.image}">
+        </div>
+
+        
+
+        <div class="productName">
+          ${product.name}
+        </div>
+
+        
+        <div class="addToCartContainer">
+          <div class="quantityContainer">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+          <div class="addtoCartButtonContainer">
+            <button class="addCartButton" data-product-id="${product.id}">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+document.querySelector('.newProductsGrid').innerHTML = productsHTML;
+
+  
+});
+
+
